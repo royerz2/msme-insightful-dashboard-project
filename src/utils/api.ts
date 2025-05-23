8b1041f299bdfa8cfa2fbdb0e22db1258ef3ae7c
royerz2 <royerz.2002@gmail.com>
2025-05-24 01:01:59 +0200

    update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'http://localhost:5001/api';
 
 export const fetchWithErrorHandling = async <T>(endpoint: string): Promise<T> => {
   try {
